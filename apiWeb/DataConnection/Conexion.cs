03e7a29c22f23e5ba820c33a0e327e0614d043b4
Miguel Eduardo Clavijo <miguel.e.c@outlook.com>
2022-11-02 08:21:11 -0500

    Initial commit

---
+++ apiWeb/DataConnection/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.SqlClient;
+namespace apiWeb.DataConnection
+{
+    public class Conexion
+    {
+        SqlConnection Connection = new SqlConnection(@"server=localhost; database=pruebaSara;Trusted_Connection= True;" +
+            "MultipleActiveResultSets= True;TrustServerCertificate= False;Encrypt= False");
+        public SqlConnection getcn
+        {
+            get { return Connection; }
+        }
+    }
+}
